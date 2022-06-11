3adb738f23cda83991df8e13821a55bbff86dd7e
pritish r das <pritish.r.das@apiseroinc.onmicrosoft.com>
2022-06-11 23:03:57 +0530

    Removing strict mode and using localStorage in useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
@@ -19,12 +19,24 @@ import ContactList from "./ContactList";
 // ]
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
     // Yaha pe ek naya array bann raha hai and usme contacts unpack hua ek ek karke and ek aur item bass judd gaya
     setContacts([...contacts, contact]);
   }
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrieveContacts) {
+      setContacts(retrieveContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className = "ui container">
