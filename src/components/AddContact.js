a9d1ab3c95a486d6838ed2492d76f027a545ea6f
pritish r das <pritish.r.das@apiseroinc.onmicrosoft.com>
2022-06-11 20:03:55 +0530

    AddContact props data up and down

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const AddContact = () => {
+const AddContact = (props) => {
+
+    useEffect(() => {
+        console.log('Component connected to DOM');
+    }, []);
+
+    useEffect(() => {
+        console.log('Component rendered');
+    });
+
+    const [contact, setContact] = useState({ name: '', email: ''});
+
+    const handleNameChnage = (event) => {
+        console.log('handleNameChange');
+        setContact({ ...contact, name: event.target.value});
+    }
+
+    const handleEmailChange = (event) => {
+        console.log('handleEmailChange');
+        setContact({ ...contact, email: event.target.value});
+    }
+
+    const add = (event) => {
+        event.preventDefault();
+        if(contact.name === "" || contact.email === "") {
+            alert("All fields are mandatory");
+            return;
+        }
+
+        props.addContactHandler(contact);
+    }
+
     return (
         <div className = "ui main">
             <h2>Add Contact</h2>
-            <form className = "ui form">
+            <form className = "ui form" onSubmit = {add}>
                 <div className = "field">
                     <label>Name</label>
-                    <input type="text" name="name" placeholder="Name" />
+                    <input 
+                        type = "text"
+                        name = "name"
+                        placeholder = "Name"
+                        onChange ={handleNameChnage}
+                        value = {contact.name} />
                 </div>
                 <div className = "field">
                     <label>Email</label>
-                    <input type="text" name="email" placeholder="Email" />
+                    <input 
+                        type = "text"
+                        name = "email"
+                        placeholder = "Email"
+                        onChange = {handleEmailChange}
+                        value = {contact.email} />
                 </div>
                 <button className = "ui button blue">Add</button>
             </form>
